9804ef390a4c763160985725591f4950bd1d6e4e
Uenes Lucio Vilaça Ferreira <uenes.ferreira@ans.gov.br>
2017-09-15 16:22:18 -0300

    Primeira versão da aplicação. Verifica se o branch não está atualizado com o servidor e, caso não esteja, apresenta os arquivos que devem ser atualizados

---
+++ check_up-to-date.py
@@ -0,0 +1,49 @@
+import sys
+import ctypes  
+import time
+from subprocess import Popen, PIPE
+
+
+while True:
+	# C:\\inetpub\\wwwroot\\Integracao_SIF_SEI_Sprint5
+	process = Popen(['svn', 'status', '-u', sys.argv[1]], stdout=PIPE, stderr=PIPE)
+	stdout, stderr = process.communicate()
+	lista = stdout.splitlines()
+
+	listOfServerChanges = []
+	listOfLocalChanges = []
+
+	for item in lista:
+		isServerChange = item.find('*')
+		if isServerChange != -1:
+			print item
+			listOfServerChanges.append(item)
+		else:
+			if item[0] == 'M':
+				print item
+				listOfLocalChanges.append(item)
+			
+	stringOfPathServerChanges = ''		
+	for item in listOfServerChanges:
+		indexOfPath = item.find('C:')
+		path = item[indexOfPath:len(item)]
+		lastOne = path.rfind('\\')
+		previousLastOne = path[0:lastOne].rfind('\\')
+		
+		stringOfPathServerChanges = stringOfPathServerChanges + path[previousLastOne:len(path)] + '\n'
+		 
+		
+	listOfPathLocalChanges = []	
+	for item in listOfPathLocalChanges:
+		indexOfPath = item.find('C:')
+		path = item[indexOfPath:len(item)]		
+
+
+	if len(listOfServerChanges) == 0:
+		result = ctypes.windll.user32.MessageBoxW(0, u'Novos commits realizados no branch \n \n' + stringOfPathServerChanges, u'ALERTA', 1)
+		#if result == 1:
+		#	process = Popen(['svn', 'update', sys.argv[1]], stdout=PIPE, stderr=PIPE)
+		#	stdout, stderr = process.communicate()
+		#	resultUpdate = ctypes.windll.user32.MessageBoxW(0, u' Resultado do Update : ' + '\n \n' + stdout, u'UPDATE', 1)
+		
+	time.sleep(60)
