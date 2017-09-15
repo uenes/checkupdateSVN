2211dae8eccbff2c32db35a1cee6996d6457d9f9
Uenes Lucio Vilaça Ferreira <uenes.ferreira@ans.gov.br>
2017-09-15 18:00:54 -0300

    Modularização dos blocos para melhorar legibilidade

--- check_up-to-date.py
+++ check_up-to-date.py
@@ -3,6 +3,35 @@ import ctypes
 import time
 from subprocess import Popen, PIPE
 
+# **************** BEGIN FUNCTIONS ****************
+def getListOfServerChanges (lista):
+	result = []
+	for item in lista:
+		isServerChange = item.find('*')
+		if isServerChange != -1:
+			result.append(item)
+	return result
+
+def getStrOfPathServerChanges (listOfServerChanges):
+	result = ''
+	for item in listOfServerChanges:
+		indexOfPath = item.find('C:')
+		path = item[indexOfPath:len(item)]
+		lastOne = path.rfind('\\')
+		previousLastOne = path[0:lastOne].rfind('\\')
+		result = result + path[previousLastOne:len(path)] + '\n'
+	return result
+
+def messageOfNonUpToDate (stringOfPathServerChanges):
+	message = u'Novos commits realizados no branch \n \n' + stringOfPathServerChanges + '\n \n Atualizar a copia local? \n \n Nao : Proxima verificacao em 8 horas'
+	return ctypes.windll.user32.MessageBoxW(0, message , u'ALERTA', 0x3|0x20|0x10000|0x40000)	
+
+def messageUpdateResult (updateResult):
+	message = u' Resultado do Update : \n \n ' + updateResult
+	ctypes.windll.user32.MessageBoxW(0, message , u'ALERTA', 0x0|0x40|0x10000|0x40000)
+
+# **************** END FUNCTIONS ****************
+	
 timeOfDelay = 300
 
 while True:
@@ -11,43 +40,19 @@ while True:
 	stdout, stderr = process.communicate()
 	lista = stdout.splitlines()
 
-	listOfServerChanges = []
-	listOfLocalChanges = []
-
-	for item in lista:
-		isServerChange = item.find('*')
-		if isServerChange != -1:
-			listOfServerChanges.append(item)
-		else:
-			if item[0] == 'M':
-				listOfLocalChanges.append(item)
+	listOfServerChanges = getListOfServerChanges(lista)
 			
-	stringOfPathServerChanges = ''		
-	for item in listOfServerChanges:
-		indexOfPath = item.find('C:')
-		path = item[indexOfPath:len(item)]
-		lastOne = path.rfind('\\')
-		previousLastOne = path[0:lastOne].rfind('\\')
-		stringOfPathServerChanges = stringOfPathServerChanges + path[previousLastOne:len(path)] + '\n'
-		 
-		
-	#listOfPathLocalChanges = []	
-	#for item in listOfPathLocalChanges:
-	#	indexOfPath = item.find('C:')
-	#	path = item[indexOfPath:len(item)]		
+	stringOfPathServerChanges = getStrOfPathServerChanges(listOfServerChanges)			
 
-
-	if len(listOfServerChanges) == 0:
-		message = u'Novos commits realizados no branch \n \n' + stringOfPathServerChanges + '\n \n Atualizar a copia local? \n \n Cancel : Proxima verificacao em 8 horas'
-		result = ctypes.windll.user32.MessageBoxW(0, message , u'ALERTA', 0x3|0x20|0x10000|0x40000)
-		print result
-		if result == 2:
+	if len(listOfServerChanges) > 0:
+		result = messageOfNonUpToDate(stringOfPathServerChanges)
+		if result == 7:
 			timeOfDelay = 28800
 		if result == 6:
 			process = Popen(['svn', 'update', sys.argv[1]], stdout=PIPE, stderr=PIPE)
 			stdout, stderr = process.communicate()
-			message = u' Resultado do Update : \n \n blablabla' + stdout
-			ctypes.windll.user32.MessageBoxW(0, message , u'ALERTA', 0x0|0x40|0x10000|0x40000)
+			messageUpdateResult(stdout)
 		
 	time.sleep(timeOfDelay)
 	timeOfDelay = 300
+
