352c1723099b5d21a2bbaa9a126d00ab44982b3a
Uenes Lucio Vilaça Ferreira <uenes.ferreira@ans.gov.br>
2017-09-18 15:28:23 -0300

    Corrigido o erro que ocorria após aceitar atualizar o projeto

--- check_up-to-date.py
+++ check_up-to-date.py
@@ -1,7 +1,10 @@
+# encoding: utf-8
+
 import sys
 import ctypes  
 import time
 from subprocess import Popen, PIPE
+
 
 # **************** BEGIN FUNCTIONS ****************
 def getListOfServerChanges (lista):
@@ -23,16 +26,21 @@ def getStrOfPathServerChanges (listOfServerChanges):
 	return result
 
 def messageOfNonUpToDate (stringOfPathServerChanges):
-	message = u'Novos commits realizados no branch \n \n' + stringOfPathServerChanges + '\n \n Atualizar a copia local? \n \n Nao : Proxima verificacao em 8 horas'
+	message = u'Novos commits realizados no branch \n \n' + stringOfPathServerChanges + '\n \n Atualizar a cópia local? \n \n Não : Próxima verificação em 8 horas'
 	return ctypes.windll.user32.MessageBoxW(0, message , u'ALERTA', 0x3|0x20|0x10000|0x40000)	
 
-def messageUpdateResult (updateResult):
-	message = u' Resultado do Update : \n \n ' + updateResult
+def messageUpdateResult (updateResult, stderr):
+	#if updateResult is not None:
+	message = u'Update realizado com sucesso! ' 
+	#else:
+	#	message = u' Resultado do Update : \n \n ' + type(updateResult)
 	ctypes.windll.user32.MessageBoxW(0, message , u'ALERTA', 0x0|0x40|0x10000|0x40000)
 
 # **************** END FUNCTIONS ****************
 	
 timeOfDelay = 300
+reload(sys)  # Reload does the trick!
+sys.setdefaultencoding('UTF8')
 
 while True:
 	# C:\\inetpub\\wwwroot\\Integracao_SIF_SEI_Sprint5
@@ -51,7 +59,7 @@ while True:
 		if result == 6:
 			process = Popen(['svn', 'update', sys.argv[1]], stdout=PIPE, stderr=PIPE)
 			stdout, stderr = process.communicate()
-			messageUpdateResult(stdout)
+			messageUpdateResult(stdout, stderr)
 		
 	time.sleep(timeOfDelay)
 	timeOfDelay = 300
